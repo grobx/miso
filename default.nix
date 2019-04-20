767fc3f518fb84d71e627036a9c5bc9b32972f93
Giuseppe Roberti <3105723+roberti42@users.noreply.github.com>
2019-04-20 21:30:40 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,9 +1,4 @@
-{ pkgs ? import ((import <nixpkgs> {}).fetchFromGitHub {
-    owner = "NixOS";
-    repo = "nixpkgs";
-    rev = "a0aeb23";
-    sha256 = "04dgg0f2839c1kvlhc45hcksmjzr8a22q1bgfnrx71935ilxl33d";
-  }){}
+{ pkgs ? import <nixpkgs> {}
 , haddock ? true
 }:
 let
